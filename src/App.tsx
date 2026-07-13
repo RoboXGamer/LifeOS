9bde0bac393e7e60ee97de121aaf766284f3f9ec
RoboXGamer <roboxgamer52@gmail.com>
2026-07-13 17:57:55 +0530

    draft 2 - Item implemented basic 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,72 @@ import { createSignal, For } from "solid-js";
 import "./App.css";
 import { nanoid } from "nanoid";
 
-type Todo = {
+type Item = {
   id: string;
   text: string;
+  areaId: string | null;
+  createdAt: string;
+  updatedAt: string;
 };
 
 function App() {
-  const [todos, setTodos] = createSignal<Todo[]>([]);
+  const [items, setItems] = createSignal<Item[]>([]);
 
   const onSubmit = (e: Event) => {
     e.preventDefault();
     const form = e.currentTarget as HTMLFormElement;
-    const todo = new FormData(form).get("todo")?.toString() ?? "";
-    if (!todo) return;
-    const currentTodo = { text: todo, id: nanoid() };
-    setTodos([...todos(), currentTodo]);
+    const text = new FormData(form).get("item")?.toString() ?? "";
+    if (!text) return;
+    const currentItem = { text, id: nanoid(), areaId: null, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() };
+    setItems([...items(), currentItem]);
     form.reset();
   };
 
   const onDelete = (id: string) => {
-    setTodos(todos().filter((t) => t.id !== id));
+    setItems(items().filter((t) => t.id !== id));
   };
 
   return (
     <>
       <main class="center">
-        <section id="todo-form-container">
+        <section id="item-form-container">
           <form onSubmit={onSubmit}>
             <input
               autocomplete="off"
-              id="todo"
-              name="todo"
+              id="item"
+              name="item"
               type="text"
               placeholder="What needs to be done?"
             />
-            <button type="submit">Add Todo</button>
+            <button type="submit">Add Item</button>
           </form>
         </section>
         <hr />
-        <section id="todo-list-container">
-          <ol>
-            <For each={todos()} keyed={(t) => t.id}>
-              {(todo, index) => (
-                <li>
-                  <span>
-                    {`${index() + 1}. `}
-                    {todo().text}
-                  </span>
-                  <button onClick={() => onDelete(todo().id)}>Delete</button>
-                </li>
-              )}
-            </For>
-          </ol>
-        </section>
+        <Inbox items={items} onDelete={onDelete} />
       </main>
     </>
+  );
+}
+
+function Inbox(props: { items: () => Item[]; onDelete: (id: string) => void }) {
+  const [inboxItems] = createSignal(() => props.items().filter((item) => item.areaId === null));
+
+  return (
+    <section id="item-list-container">
+      <ol>
+        <For each={inboxItems()} keyed={(t) => t.id}>
+          {(item, index) => (
+            <li>
+              <span>
+                {`${index() + 1}. `}
+                {item().text}
+              </span>
+              <button onClick={() => props.onDelete(item().id)}>Delete</button>
+            </li>
+          )}
+        </For>
+      </ol>
+    </section>
   );
 }
 
